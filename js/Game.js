77a0431af27cb175c2314969de92ea558558b98c
Luci Peterson <lucijpeterson@gmail.com>
2019-07-08 13:20:22 -0700

    Skeleton, functionality to start game added
    Drafted code skeleton, added functionality to the game which chooses a phrase from the array and displays the spaces for the phrase on the page when you hit the start button.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,54 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Game.js */
+//Treehouse FSJS Techdegree - Project 4 - OOP Game App - Game.js
+
+//PAGE ELEMENTS DEFINED
+const startScreenOverlay = document.getElementById("overlay");
+
+//CLASSES
+class Game {
+    constructor(){
+        this.missed = 0; 
+        this.phrases = [
+            "The bigger they are the harder they fall",
+            "Better safe than sorry",
+            "Misery loves company",
+            "Knowledge is power",
+            "One bad apple spoils the whole bunch"]; 
+        this.activePhrase = null;
+    }
+
+    getRandomPhrase(){
+        const phrase = new Phrase(this.phrases[Math.floor(Math.random()*this.phrases.length)]);
+        return phrase
+    }
+
+    startGame(){
+        startScreenOverlay.style.display = "none";
+        this.activePhrase = this.getRandomPhrase(); 
+        this.activePhrase.addPhraseToDisplay();
+    }
+
+    handleInteraction(choice){
+        choice.addEventListener('click', 'disable');
+        if ("choice not in phrase") {
+            choice.addClass('wrong');
+            removeLife();
+        } else {
+            choice.addClass('chosen');
+            showMatchedLetter(phrase);
+        }
+    checkForWin();
+    if (win === true) {gameOver()}
+    }
+
+    removeLife(){
+        live.remove();
+        lost.add();
+        if (lostHearts.count > 4) {gameOver()};
+    }
+    checkForWin(){
+        let win = false;
+        let correctLetters = [];
+        if (correctLetters in phraseLetters){win = true} else win = false;
+    }
+    gameOver(){"display updated startScreenOverlay"}
+};
--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * app.js */
+//Treehouse FSJS Techdegree - Project 4 - OOP Game App - app.js
+
+//DOM ELEMENTS
+const startGameButton = document.getElementById('btn__reset');
+const keyboard = document.getElementById('qwerty');
+
+//NEW GAME INSTANTIATED
+// const game = new Game();
+// game.phrases.forEach((phrase, index) => {
+//     console.log(`Phrase ${index} - phrase: ${phrase}`);
+//     });
+// game.startGame();
+
+//FUNCTIONS
+function reset(){
+    const game = new Game();
+    game.startGame();
+};
+
+//EVENT HANDLERS
+startGameButton.addEventListener('click', reset);
+
+//TEST
+// const game = new Game();
+// game.startGame();
+// console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
